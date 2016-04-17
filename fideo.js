995073e434a3f5ee82e938dd68931a43db5b55b3
Wayne Durack <wayne.durack@gmail.com>
2016-04-17 17:11:11 +0100

    Added class to created wrapper element

--- fideo.js
+++ fideo.js
@@ -9,35 +9,36 @@
 				i;
 
 			o = options;
-			
+
 			// Target is a single HTML element
 			if (target.nodeName) {
 				return this.enhance(target);
 			}
-			
+
 			// Check the target is an array
 			if (!target.length) { return; }
-			
-			
+
+
 			// Loop through the nodes
 			for (i = 0; i < target.length; i++) {
 				this.enhance(target[i]);
 			}
-			
+
 		}
-		
+
 		this.enhance = function (target) {
 			var wrapper = document.createElement("div"),
 				guide = this.makeGuide(),
 				parent = target.parentNode;
 
+			wrapper.classList.add("jsFideo");
 			wrapper.appendChild(guide);
 			parent.insertBefore(wrapper, target);
 			target.style.cssText = "position: absolute; width: 100%; height: 100%; top: 0; left: 0;"
 			wrapper.appendChild(target);
 			wrapper.style.cssText = "position: relative;";
 		}
-		
+
 		// create the guide element
 		this.makeGuide = function () {
 			var guide = document.createElement("img");
@@ -45,9 +46,9 @@
 			guide.style.cssText = "display: block; width: 100%; height: auto;";
 			return guide;
 		}
-		
+
 		this.root = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAA"
-		
+
 		// base64 image datauri for each aspect ratio without the common header
 		this.guides = {
 			"2.35:1":"OsAAABkCAMAAABQMjfqAAAAA1BMVEX///+nxBvIAAAAAXRSTlMAQObYZgAAAC1JREFUeNrtwTEBAAAAwiD7p14MH2AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAwE1cMAAB6Ne5JQAAAABJRU5ErkJggg==",
@@ -57,13 +58,13 @@
 			"4:3": "AQAAAADCAMAAACDKl70AAAAA1BMVEX///+nxBvIAAAAAXRSTlMAQObYZgAAAApJREFUeAFjQAUAAA8AAeTdOmQAAAAASUVORK5CYII=",
 			"1:1": "AEAAAABCAQAAAC1HAwCAAAAC0lEQVQI12P4zwAAAgEBAKrChTYAAAAASUVORK5CYII="
 		}
-		
+
 		// Get the correct datauri string
 		this.getGuide = function (size) {
 			return this.root + (this.guides[size] ? this.guides[size] : this.guides[defaultSize]);
 		}
-		
+
 		this.Init(options);
-		
+
 	});
 }());
